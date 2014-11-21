skltp-incubator/anslutningsverktyg

commit 78f54ada21bb15705998360695b2273a51b73fdf

AV-31: using backend for service contracts

app/scripts/controllers/connectServiceProducer.js
@@ -44,8 +44,8 @@ angular.module('avApp')
         enableSelectAll: true,
         multiSelect: true,
         columnDefs: [
-          {name: 'Namn', field: 'name'},
-          {name: 'version'}
+          {name: 'Namn', field: 'namn'},
+          {name: 'version', field: 'getVersion()'}
         ]
       };
 
@@ -92,8 +92,12 @@ angular.module('avApp')
           var serviceDomainId = $scope.selectedServiceDomain.id;
           $scope.connectServiceProducerRequest.serviceDomain = $scope.selectedServiceDomain;
           ServiceContract.listContracts(serviceComponentId, environmentId, serviceDomainId).then(function (contracts) {
-            //$scope.existingLogicalAddresses = contracts.existingLogicalAddresses;
-            $scope.gridOptions.data = contracts.serviceContracts;
+            $scope.gridOptions.data = contracts;
+            _.each($scope.gridOptions.data, function(contractData) { //ui-grid doesn't seem to support composite field values in any other way
+              contractData.getVersion = function() {
+                return this.majorVersion + '.' + this.minorVersion;
+              }
+            })
           });
           LogicalAddress.getLogicalAddressesForServiceDomain(serviceDomainId).then(function(logicalAddresses) {
             $scope.existingLogicalAddresses = logicalAddresses;

app/scripts/factories/serviceContract.js
@@ -1,42 +1,23 @@
 'use strict';
 angular.module('avApp')
-  .factory('ServiceContract', ['$q',
-    function ($q) {
+  .factory('ServiceContract', ['$q', '$http',
+    function ($q, $http) {
     return {
       listContracts: function(serviceComponentId, environmentId, serviceDomainId) {
         console.log('serviceComponentId: ' + serviceComponentId + ', environmentId: ' + environmentId + ', serviceDomainId: ' + serviceDomainId);
         var deferred = $q.defer();
-        var response = {
-          serviceContracts: [
-            {
-              id: 'serviceContract1',
-              name: 'tjanstekontrakt1',
-              version: 1
-            },
-            {
-              id: 'serviceContract2',
-              name: 'tjanstekontrakt2',
-              version: 1
-            },
-            {
-              id: 'serviceContract3',
-              name: 'tjanstekontrakt3',
-              version: 1
-            }
-          ],
-          existingLogicalAddresses: [
-          {
-            id: 'hsaId5',
-            name: 'Varberg'
-          },
-          {
-            id: 'hsaId7',
-            name: 'Vallda'
+        $http.get('http://localhost:8080/anslutningsverktyg/api/serviceContracts', {
+          params: {
+            hsaId: serviceComponentId,
+            environmentId: environmentId,
+            serviceDomainId: serviceDomainId
           }
-        ]};
-
-        deferred.resolve(response);
-        return deferred.promise;
+        }).success(function(data) {
+          console.log(data);
+          deferred.resolve(data);
+        }).error(function(data, status, headers) { //TODO: handle errors
+          deferred.reject();
+        });
       }
     };
   }]);

commit 4b63b286f99b8b1a9a5ffd05b47deec1add8a845

fixed a few bugs introduced in 78f54ada21bb15705998360695b2273a51b73fdf

app/scripts/controllers/connectServiceProducer.js
@@ -245,14 +245,24 @@ angular.module('avApp')
 
       var updateSelectedServiceContracts = function (row) {
         $log.info(row);
-        var where = {'id': row.entity.id};
-        if (row.isSelected && !_.find($scope.selectedServiceContracts, where)) {
-          $scope.selectedServiceContracts.push(row.entity);
-        } else if (!row.isSelected && _.find($scope.selectedServiceContracts, where)) {
-          _.remove($scope.selectedServiceContracts, where);
+        var serviceContract = _cleanServiceContract(row.entity);
+        var serviceContractIdentifier = {
+          namnrymd: serviceContract.namnrymd,
+          majorVersion: serviceContract.majorVersion,
+          minorVersion: serviceContract.minorVersion
+        };
+        if (row.isSelected && !_.find($scope.selectedServiceContracts, serviceContractIdentifier)) {
+          $scope.selectedServiceContracts.push(serviceContract);
+        } else if (!row.isSelected && _.find($scope.selectedServiceContracts, serviceContractIdentifier)) {
+          _.remove($scope.selectedServiceContracts, serviceContractIdentifier);
         }
         $scope.connectServiceProducerRequest.serviceContracts = $scope.selectedServiceContracts;
         $log.info($scope.selectedServiceContracts);
+      };
+
+      var _cleanServiceContract = function(serviceContract) {
+        delete serviceContract.getVersion;
+        return serviceContract;
       };
 
       var reset = function () {

app/views/serviceProducer/connect.html
@@ -287,7 +287,7 @@
 
   <div ng-repeat="contract in connectServiceProducerRequest.serviceContracts">
     <div class="jumbotron">
-      <h3>{{contract.name}}</h3>
+      <h3>{{contract.namn}} {{contract.majorVersion}}.{{contract.minorVersion}}</h3>
       <div class="row">
 
         <div class="col-md-3">
@@ -304,7 +304,7 @@
 
       </div>
 
-      <h4 ng-show="individualLogicalAddress">Välj logiska adresser till {{contract.name}}</h4>
+      <h4 ng-show="individualLogicalAddress">Välj logiska adresser till {{contract.namn}} {{contract.majorVersion}}.{{contract.minorVersion}}</h4>
 
       <div ng-show="individualLogicalAddress" class="row">
         <div class="col-md-12">